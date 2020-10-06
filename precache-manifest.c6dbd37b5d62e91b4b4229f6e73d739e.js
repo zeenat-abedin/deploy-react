1fa90d23de66de12bb406f02c01fab94bd62722a
zeenat-abedin-au3 <zeenu.23@gmail.com>
2020-10-06 11:47:18 +0530

    Updates

---
+++ precache-manifest.c6dbd37b5d62e91b4b4229f6e73d739e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffaf6e19a6f53e03614fb0c8e4e5ad65",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "5e6eab744dbf55244387",
+    "url": "/deploy-react/static/css/main.6837718b.chunk.css"
+  },
+  {
+    "revision": "fda7aeacd84abf9db942",
+    "url": "/deploy-react/static/js/2.5de09e7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.5de09e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6eab744dbf55244387",
+    "url": "/deploy-react/static/js/main.ffcdec2f.chunk.js"
+  },
+  {
+    "revision": "428cc438c8f1e1fc245c",
+    "url": "/deploy-react/static/js/runtime-main.1afb15cd.js"
+  }
+]);
